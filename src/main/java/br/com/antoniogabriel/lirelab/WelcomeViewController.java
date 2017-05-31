9c3f859f157156e53469e66014a78944591a5ce1
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-05-31 20:30:43 -0300

    Open create collection window at button click

--- src/main/java/br/com/antoniogabriel/lirelab/WelcomeViewController.java
+++ src/main/java/br/com/antoniogabriel/lirelab/WelcomeViewController.java
@@ -1,17 +1,23 @@
 package br.com.antoniogabriel.lirelab;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class WelcomeViewController {
     public static final String CREATE_COLLECTION = "Create Collection";
 
-    public void openCreateCollectionDialog(ActionEvent event) {
+    public void openCreateCollectionDialog(ActionEvent event) throws IOException {
         Stage stage = new Stage();
         stage.setTitle(CREATE_COLLECTION);
-        stage.setScene(new Scene(new StackPane(), 600, 400));
+        Parent root = FXMLLoader.load(
+                getClass().getClassLoader().getResource(
+                        "br/com/antoniogabriel/lirelab/collection/create-collection.fxml"));
+        stage.setScene(new Scene(root, 600, 400));
         stage.centerOnScreen();
         stage.show();
     }
---
+++ src/main/java/br/com/antoniogabriel/lirelab/collection/CreateCollectionController.java
@@ -0,0 +1,77 @@
+package br.com.antoniogabriel.lirelab.collection;
+
+import br.com.antoniogabriel.lirelab.Feature;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreateCollectionController implements Initializable {
+
+    @FXML
+    private TableView<FeatureModel> featuresTable;
+    @FXML
+    private TableColumn<FeatureModel, Boolean> selectedCol;
+    @FXML
+    private TableColumn<FeatureModel, String> nameCol;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        selectedCol.setCellValueFactory(new PropertyValueFactory<FeatureModel, Boolean>("selected"));
+        nameCol.setCellValueFactory(new PropertyValueFactory<FeatureModel, String>("name"));
+        selectedCol.setCellFactory(CheckBoxTableCell.forTableColumn(selectedCol));
+        featuresTable.setItems(getFeatureItems());
+    }
+
+    private ObservableList<FeatureModel> getFeatureItems() {
+        ObservableList<FeatureModel> items = FXCollections.observableArrayList();
+        for (Feature feature : Feature.values()) {
+            items.add(new FeatureModel(feature));
+        }
+        return items;
+    }
+
+    public class FeatureModel {
+        private final SimpleBooleanProperty selected;
+        private final SimpleStringProperty name;
+
+        public FeatureModel(Feature feature) {
+            this.selected = new SimpleBooleanProperty(false);
+            this.name = new SimpleStringProperty(feature.name());
+        }
+
+        public boolean isSelected() {
+            return selected.get();
+        }
+
+        public SimpleBooleanProperty selectedProperty() {
+            return selected;
+        }
+
+        public void setSelected(boolean selected) {
+            this.selected.set(selected);
+        }
+
+        public String getName() {
+            return name.get();
+        }
+
+        public SimpleStringProperty nameProperty() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name.set(name);
+        }
+    }
+
+}
