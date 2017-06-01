92521a09724beda83443e75ab95d6ffc52d90b9b
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-05-31 23:44:11 -0300

    Add Browse button to trigger DirectoryChooser
    The DirectoryChooser is necessary for the user to select the directory where the images to be indexed are stored

--- src/main/java/br/com/antoniogabriel/lirelab/collection/CreateCollectionController.java
+++ src/main/java/br/com/antoniogabriel/lirelab/collection/CreateCollectionController.java
@@ -5,18 +5,25 @@ import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Window;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CreateCollectionController implements Initializable {
 
+    @FXML
+    private TextField pathToImages;
     @FXML
     private TableView<FeatureModel> featuresTable;
     @FXML
@@ -38,6 +45,16 @@ public class CreateCollectionController implements Initializable {
             items.add(new FeatureModel(feature));
         }
         return items;
+    }
+
+    public void chooseDir(ActionEvent event) {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Select the directory that contains the images");
+        Window window = featuresTable.getScene().getWindow();
+        File dir = chooser.showDialog(window);
+        if (dir != null) {
+            pathToImages.setText(dir.getAbsolutePath());
+        }
     }
 
     public class FeatureModel {
