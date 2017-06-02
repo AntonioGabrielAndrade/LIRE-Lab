cf3741f99be58070ed0e87bf4bded7e55774e2e8
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-06-01 21:39:27 -0300

    Set Create Collection window as modal owned by main UI

--- src/main/java/br/com/antoniogabriel/lirelab/WelcomeViewController.java
+++ src/main/java/br/com/antoniogabriel/lirelab/WelcomeViewController.java
@@ -1,9 +1,12 @@
 package br.com.antoniogabriel.lirelab;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,9 +14,14 @@ import java.io.IOException;
 public class WelcomeViewController {
     public static final String CREATE_COLLECTION = "Create Collection";
 
+    @FXML
+    private Button createCollectionButton;
+
     public void openCreateCollectionDialog(ActionEvent event) throws IOException {
         Stage stage = new Stage();
         stage.setTitle(CREATE_COLLECTION);
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(createCollectionButton.getScene().getWindow());
         Parent root = FXMLLoader.load(
                 getClass().getClassLoader().getResource(
                         "br/com/antoniogabriel/lirelab/collection/create-collection.fxml"));
