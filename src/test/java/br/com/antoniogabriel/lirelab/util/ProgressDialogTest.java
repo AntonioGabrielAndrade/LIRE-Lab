72b73212918aa439f20474e3379b2f3508bec73c
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-06-04 00:04:18 -0300

    Increase timeouts for wait-async verifications in tests

--- src/test/java/br/com/antoniogabriel/lirelab/util/ProgressDialogTest.java
+++ src/test/java/br/com/antoniogabriel/lirelab/util/ProgressDialogTest.java
@@ -51,43 +51,43 @@ public class ProgressDialogTest extends ApplicationTest {
     public void shouldUpdateProgress() throws Exception {
         new Thread(task).start();
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 progressBar.progressProperty().isEqualTo(0.1, 0));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 progressBar.progressProperty().isEqualTo(0.2, 0.05));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 progressBar.progressProperty().isEqualTo(0.3, 0.05));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 progressBar.progressProperty().isEqualTo(0.4, 0.0));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 progressBar.progressProperty().isEqualTo(0.5, 0.0));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 progressBar.progressProperty().isEqualTo(0.6, 0.0));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 progressBar.progressProperty().isEqualTo(0.7, 0.0));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 progressBar.progressProperty().isEqualTo(0.8, 0.0));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 progressBar.progressProperty().isEqualTo(0.9, 0.0));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 progressBar.progressProperty().isEqualTo(1.0, 0.0));
 
@@ -98,43 +98,43 @@ public class ProgressDialogTest extends ApplicationTest {
     public void shouldUpdateMessage() throws Exception {
         new Thread(task).start();
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 message.textProperty().isEqualTo("Progress: 1 of 10"));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 message.textProperty().isEqualTo("Progress: 2 of 10"));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 message.textProperty().isEqualTo("Progress: 3 of 10"));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 message.textProperty().isEqualTo("Progress: 4 of 10"));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 message.textProperty().isEqualTo("Progress: 5 of 10"));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 message.textProperty().isEqualTo("Progress: 6 of 10"));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 message.textProperty().isEqualTo("Progress: 7 of 10"));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 message.textProperty().isEqualTo("Progress: 8 of 10"));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 message.textProperty().isEqualTo("Progress: 9 of 10"));
 
-        WaitForAsyncUtils.waitFor(2,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 message.textProperty().isEqualTo("Progress: 10 of 10"));
 
@@ -148,7 +148,7 @@ public class ProgressDialogTest extends ApplicationTest {
                 TimeUnit.SECONDS,
                 progressBar.progressProperty().isEqualTo(1.0, 0));
 
-        WaitForAsyncUtils.waitFor(3,
+        WaitForAsyncUtils.waitFor(10,
                 TimeUnit.SECONDS,
                 button.disabledProperty().not());
     }
