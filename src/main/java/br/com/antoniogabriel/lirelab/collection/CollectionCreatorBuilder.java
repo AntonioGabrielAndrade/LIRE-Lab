381ea8df01a8b9393d6dae645753192dd8ed490c
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-06-03 22:18:19 -0300

    Add CreateCollectionBuilder to help creating CollectionCreator objects

---
+++ src/main/java/br/com/antoniogabriel/lirelab/collection/CollectionCreatorBuilder.java
@@ -0,0 +1,39 @@
+package br.com.antoniogabriel.lirelab.collection;
+
+import br.com.antoniogabriel.lirelab.Feature;
+
+import java.util.ArrayList;
+
+public class CollectionCreatorBuilder {
+    private CollectionCreator creator;
+    private ArrayList<Feature> features;
+    private String imagesDir;
+    private String indexDir;
+
+    public CollectionCreatorBuilder aCreator() {
+        creator = new CollectionCreator();
+        return this;
+    }
+
+    public CollectionCreatorBuilder indexForFeatures(ArrayList<Feature> features) {
+        this.features = features;
+        return this;
+    }
+
+    public CollectionCreatorBuilder readImagesFrom(String imagesDir) {
+        this.imagesDir = imagesDir;
+        return this;
+    }
+
+    public CollectionCreatorBuilder openIndexIn(String indexDir) {
+        this.indexDir = indexDir;
+        return this;
+    }
+
+    public CollectionCreator build() {
+        creator.setFeatures(features);
+        creator.setImagesDir(imagesDir);
+        creator.setIndexDir(indexDir);
+        return creator;
+    }
+}
