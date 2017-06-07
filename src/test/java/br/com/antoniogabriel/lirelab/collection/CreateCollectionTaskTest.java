0b30d94beb65d44c12af23e02c67742f608635a7
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-06-06 22:43:19 -0300

    Use Mockito annotations

--- src/test/java/br/com/antoniogabriel/lirelab/collection/CreateCollectionTaskTest.java
+++ src/test/java/br/com/antoniogabriel/lirelab/collection/CreateCollectionTaskTest.java
@@ -1,17 +1,20 @@
 package br.com.antoniogabriel.lirelab.collection;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CreateCollectionTaskTest {
 
-    private CollectionCreator creator;
+    @Mock private CollectionCreator creator;
 
     @Test
     public void shouldSetItselfAsCallback() throws Exception {
-        creator = mock(CollectionCreator.class);
         CreateCollectionTask task = new CreateCollectionTask(creator);
 
         verify(creator).setCallback(task);
@@ -19,7 +22,6 @@ public class CreateCollectionTaskTest {
 
     @Test
     public void shouldCreateCollectionStepByStep() throws Exception {
-        creator = mock(CollectionCreator.class);
         CreateCollectionTask task = new CreateCollectionTask(creator);
 
         task.call();
