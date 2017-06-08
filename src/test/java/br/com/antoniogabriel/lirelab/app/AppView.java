ae93e272fa124e751768fe67f08f0cae6328d829
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-06-08 12:57:40 -0300

    Use MenuView in AppView

--- src/test/java/br/com/antoniogabriel/lirelab/app/AppView.java
+++ src/test/java/br/com/antoniogabriel/lirelab/app/AppView.java
@@ -3,33 +3,16 @@ package br.com.antoniogabriel.lirelab.app;
 import org.testfx.api.FxRobot;
 
 import static org.testfx.api.FxAssert.verifyThat;
-import static org.testfx.matcher.base.NodeMatchers.hasChild;
 import static org.testfx.matcher.base.NodeMatchers.isVisible;
 
 public class AppView extends FxRobot {
 
+    MenuView menuView = new MenuView();
+
     public void checkMenus() {
-        checkMenuBar();
-        checkFileMenu();
-        checkHelpMenu();
-    }
-
-    private void checkMenuBar() {
-        verifyThat("#menu-bar", isVisible());
-        verifyThat("#menu-bar", hasChild("#file-menu"));
-        verifyThat("#menu-bar", hasChild("#help-menu"));
-    }
-
-    private void checkHelpMenu() {
-        clickOn("#help-menu");
-        verifyThat("#about", isVisible());
-        clickOn("#help-menu");
-    }
-
-    private void checkFileMenu() {
-        clickOn("#file-menu");
-        verifyThat("#create-collection", isVisible());
-        clickOn("#file-menu");
+        menuView.checkMenuBar();
+        menuView.checkFileMenu();
+        menuView.checkHelpMenu();
     }
 
     void checkToolBar() {
