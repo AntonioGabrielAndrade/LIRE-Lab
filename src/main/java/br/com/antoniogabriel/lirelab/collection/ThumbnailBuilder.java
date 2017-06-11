e8f020a02b72dcb268de62ee988a57713cd81f94
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-06-11 20:59:43 -0300

    Fix error when thumnails dir already exists

--- src/main/java/br/com/antoniogabriel/lirelab/collection/ThumbnailBuilder.java
+++ src/main/java/br/com/antoniogabriel/lirelab/collection/ThumbnailBuilder.java
@@ -7,12 +7,15 @@ import net.semanticmetadata.lire.utils.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class ThumbnailBuilder {
     public void createDirectory(String thumbnailsDir) throws IOException {
-        Files.createDirectory(Paths.get(thumbnailsDir));
+        Path path = Paths.get(thumbnailsDir);
+        if(!Files.exists(path))
+            Files.createDirectory(path);
     }
 
     public List<String> getAllImagePaths(String imagesDir) throws IOException {
