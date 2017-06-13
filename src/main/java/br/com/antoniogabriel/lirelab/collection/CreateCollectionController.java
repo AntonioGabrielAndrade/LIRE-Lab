eefcab57668faa54c9c56d5c62030890b1c522a8
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-06-13 16:57:21 -0300

    Extract ViewableFeature to its own file

--- src/main/java/br/com/antoniogabriel/lirelab/collection/CreateCollectionController.java
+++ src/main/java/br/com/antoniogabriel/lirelab/collection/CreateCollectionController.java
@@ -3,8 +3,6 @@ package br.com.antoniogabriel.lirelab.collection;
 import br.com.antoniogabriel.lirelab.lire.Feature;
 import br.com.antoniogabriel.lirelab.util.ProgressDialog;
 import javafx.beans.binding.BooleanBinding;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableBooleanValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -145,46 +143,4 @@ public class CreateCollectionController implements Initializable {
         }
         return features;
     }
-
-
-    public class ViewableFeature {
-        private final SimpleBooleanProperty selected;
-        private final SimpleStringProperty name;
-        private final Feature feature;
-
-        public ViewableFeature(Feature feature) {
-            this.selected = new SimpleBooleanProperty(false);
-            this.name = new SimpleStringProperty(feature.name());
-            this.feature = feature;
-        }
-
-        public boolean isSelected() {
-            return selected.get();
-        }
-
-        public SimpleBooleanProperty selectedProperty() {
-            return selected;
-        }
-
-        public void setSelected(boolean selected) {
-            this.selected.set(selected);
-        }
-
-        public String getName() {
-            return name.get();
-        }
-
-        public SimpleStringProperty nameProperty() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name.set(name);
-        }
-
-        public Feature getFeature() {
-            return feature;
-        }
-    }
-
 }
