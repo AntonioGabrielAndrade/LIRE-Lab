45d9799c683f86d4ad6bbd1554c036a6779ba4e0
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-06-17 17:58:22 -0300

    Extract constants

--- src/main/java/br/com/antoniogabriel/lirelab/collection/PathResolver.java
+++ src/main/java/br/com/antoniogabriel/lirelab/collection/PathResolver.java
@@ -1,9 +1,12 @@
 package br.com.antoniogabriel.lirelab.collection;
 
 public class PathResolver {
-    public static final String HOME_DIRECTORY_PATH = System.getProperty("user.home");
     public static final String LIRELAB_WORK_DIRECTORY = "/lirelab";
     public static final String COLLECTIONS_DIRECTORY = "/collections";
+    public static final String INDEX_DIRECTORY = "/index";
+    public static final String THUMBNAILS_DIRECTORY = "/thumbnails";
+
+    public static final String HOME_DIRECTORY_PATH = System.getProperty("user.home");
     public static final String COLLECTIONS_PATH = HOME_DIRECTORY_PATH
                                                 + LIRELAB_WORK_DIRECTORY
                                                 + COLLECTIONS_DIRECTORY;
@@ -13,10 +16,10 @@ public class PathResolver {
     }
 
     public String getIndexDirectoryPath(String collectionName) {
-        return getCollectionPath(collectionName) + "/index";
+        return getCollectionPath(collectionName) + INDEX_DIRECTORY;
     }
 
     public String getThumbnailsDirectoryPath(String collectionName) {
-        return getCollectionPath(collectionName) + "/thumbnails";
+        return getCollectionPath(collectionName) + THUMBNAILS_DIRECTORY;
     }
 }
