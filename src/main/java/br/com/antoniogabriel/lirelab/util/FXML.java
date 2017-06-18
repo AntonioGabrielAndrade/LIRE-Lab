af05e0084d558ac9433a1ee9927baeb820db4b44
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-06-18 17:48:53 -0300

    Create abstract class FXML to facilitate fxml tests
    This class should encapsulate the logic to load a FXML into a stage, as well as retrieving the FXML associated controller. Subclasses should implement the getFXMLResourceName() method to indicate which FXML file is to be loaded. Subclasses usually will override the getTitle() method as well, to indicate the stage title

---
+++ src/main/java/br/com/antoniogabriel/lirelab/util/FXML.java
@@ -0,0 +1,58 @@
+package br.com.antoniogabriel.lirelab.util;
+
+import br.com.antoniogabriel.lirelab.collection.CreateCollectionController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
+public abstract class FXML {
+
+    private FXMLLoader loader;
+
+    @Inject
+    public FXML(FXMLLoader loader) {
+        this.loader = loader;
+        this.loader.setLocation(getClass().getResource(getFXMLResourceName()));
+    }
+
+    public void loadOwnedBy(Window owner) throws IOException {
+        Stage stage = new Stage();
+        stage.initOwner(owner);
+        loadIn(stage);
+    }
+
+    public void loadIn(Stage stage) throws IOException {
+        loadIn(stage, false);
+    }
+
+    public void loadIn(Stage stage, boolean maximized) throws IOException {
+        this.loader.setRoot(null);
+        Parent root = loader.load();
+
+        if(stage.getOwner() != null) {
+            stage.initModality(Modality.WINDOW_MODAL);
+        }
+        stage.setTitle(getTitle());
+        stage.setMaximized(maximized);
+        stage.setScene(new Scene(root));
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    public CreateCollectionController getController() throws IOException {
+        loader.load();
+        return loader.getController();
+    }
+
+    public String getTitle() {
+        return "";
+    }
+
+    public abstract String getFXMLResourceName();
+}
