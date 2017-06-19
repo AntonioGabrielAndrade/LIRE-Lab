ad3e45596d674cd998aa9af3adcbb5d3ead97814
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-06-19 13:22:05 -0300

    Add methods to PathResolver

--- src/main/java/br/com/antoniogabriel/lirelab/collection/PathResolver.java
+++ src/main/java/br/com/antoniogabriel/lirelab/collection/PathResolver.java
@@ -1,5 +1,7 @@
 package br.com.antoniogabriel.lirelab.collection;
 
+import org.jetbrains.annotations.NotNull;
+
 public class PathResolver {
     public static final String LIRELAB_WORK_DIRECTORY = "/lirelab";
     public static final String COLLECTIONS_DIRECTORY = "/collections";
@@ -8,16 +10,35 @@ public class PathResolver {
     public static final String COLLECTION_XML = "collection.xml";
 
     public static final String HOME_DIRECTORY_PATH = System.getProperty("user.home");
-    public static final String COLLECTIONS_PATH = HOME_DIRECTORY_PATH
-                                                + LIRELAB_WORK_DIRECTORY
-                                                + COLLECTIONS_DIRECTORY;
+    private String applicationBasePath = "";
+
+    public PathResolver() {
+        this.applicationBasePath = HOME_DIRECTORY_PATH;
+    }
+
+    public PathResolver(String applicationBasePath) {
+        this.applicationBasePath = applicationBasePath;
+    }
 
     public String getCollectionPath(String collectionName) {
-        return COLLECTIONS_PATH + "/" + collectionName;
+        return getCollectionsPath() + "/" + collectionName;
+    }
+
+    @NotNull
+    public String getCollectionsPath() {
+        return getApplicationBasePath() + LIRELAB_WORK_DIRECTORY + COLLECTIONS_DIRECTORY;
+    }
+
+    public String getApplicationBasePath() {
+        return applicationBasePath;
+    }
+
+    public String getWorkDirectoryPath() {
+        return getApplicationBasePath() + LIRELAB_WORK_DIRECTORY;
     }
 
     public String getCollectionXMLPath(String collectionName) {
-        return COLLECTIONS_PATH + "/" + collectionName + "/" + COLLECTION_XML;
+        return getCollectionsPath() + "/" + collectionName + "/" + COLLECTION_XML;
     }
 
     public String getIndexDirectoryPath(String collectionName) {
