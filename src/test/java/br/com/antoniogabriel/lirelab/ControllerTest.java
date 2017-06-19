e7158bef0d75b4933ea7caa6d2042029b8b6051b
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-06-19 19:55:43 -0300

    Add InjectableViewTest abstract utility test class

--- src/test/java/br/com/antoniogabriel/lirelab/ControllerTest.java
+++ src/test/java/br/com/antoniogabriel/lirelab/ControllerTest.java
@@ -1,37 +1,18 @@
 package br.com.antoniogabriel.lirelab;
 
-import br.com.antoniogabriel.lirelab.util.DependencyInjection;
 import br.com.antoniogabriel.lirelab.util.FXML;
-import com.google.inject.AbstractModule;
-import com.google.inject.Module;
 import javafx.stage.Stage;
-import org.testfx.framework.junit.ApplicationTest;
 
 import javax.inject.Inject;
-import java.util.Arrays;
-import java.util.List;
 
-public class ControllerTest<T extends FXML, C> extends ApplicationTest {
+public class ControllerTest<T extends FXML, C> extends InjectableViewTest<T> {
 
     @Inject
     protected C controller;
 
-    @Inject
-    protected T fxml;
-
-    private List<Module> modules = Arrays.asList(getBindings());
-
     @Override
     public void start(Stage stage) throws Exception {
-        DependencyInjection.init(this, modules);
+        super.start(stage);
         controller = fxml.getController();
-    }
-
-    protected AbstractModule getBindings() {
-        return new AbstractModule() {
-            @Override
-            protected void configure() {
-            }
-        };
     }
 }
---
+++ src/test/java/br/com/antoniogabriel/lirelab/InjectableViewTest.java
@@ -0,0 +1,32 @@
+package br.com.antoniogabriel.lirelab;
+
+import br.com.antoniogabriel.lirelab.util.DependencyInjection;
+import com.google.inject.AbstractModule;
+import com.google.inject.Module;
+import javafx.stage.Stage;
+import org.testfx.framework.junit.ApplicationTest;
+
+import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class InjectableViewTest<T> extends ApplicationTest {
+
+    @Inject
+    protected T fxml;
+
+    private List<Module> modules = Arrays.asList(getBindings());
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        DependencyInjection.init(this, modules);
+    }
+
+    protected AbstractModule getBindings() {
+        return new AbstractModule() {
+            @Override
+            protected void configure() {
+            }
+        };
+    }
+}
