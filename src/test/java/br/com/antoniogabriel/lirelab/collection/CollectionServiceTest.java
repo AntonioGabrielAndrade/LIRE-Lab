b5f7b0bf2a453b1a4c4c6240d023db59202233c8
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-06-19 14:17:24 -0300

    Add test to CollectionServiceTest

--- src/test/java/br/com/antoniogabriel/lirelab/collection/CollectionServiceTest.java
+++ src/test/java/br/com/antoniogabriel/lirelab/collection/CollectionServiceTest.java
@@ -1,24 +1,41 @@
 package br.com.antoniogabriel.lirelab.collection;
 
+import br.com.antoniogabriel.lirelab.lire.Feature;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CollectionServiceTest {
 
-    public static final PathResolver UNUSED_PATH_RESOLVER = null;
     private CollectionService service;
 
     @Mock private CollectionRepository collectionRepository;
 
     @Before
     public void setUp() throws Exception {
-        service = new CollectionService(UNUSED_PATH_RESOLVER, collectionRepository);
+        service = new CollectionService(new PathResolver(), collectionRepository);
+    }
+
+    @Test
+    public void shouldGetTaskToCreateCollection() throws Exception {
+        String name = "";
+        String path = "";
+        List<Feature> features = null;
+
+        CreateCollectionTask task = service.getTaskToCreateCollection(name, path, features);
+
+        assertNotNull(task);
+        assertThat(task, instanceOf(CreateCollectionTask.class));
     }
 
     @Test
