d913b952796211050edc61bd92e7a30520674a19
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-06-25 11:42:10 -0300

    Refactor CollectionGrid to use FXML

--- src/main/java/br/com/antoniogabriel/lirelab/custom/CollectionGrid.java
+++ src/main/java/br/com/antoniogabriel/lirelab/custom/CollectionGrid.java
@@ -2,6 +2,8 @@ package br.com.antoniogabriel.lirelab.custom;
 
 import br.com.antoniogabriel.lirelab.collection.Collection;
 import br.com.antoniogabriel.lirelab.collection.PathResolver;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.StackPane;
 import net.semanticmetadata.lire.utils.FileUtils;
 
@@ -11,21 +13,39 @@ import java.util.List;
 
 public class CollectionGrid extends StackPane {
 
+    private static final String COLLECTION_GRID_FXML = "collection-grid.fxml";
+
     private PathResolver resolver;
     private Collection collection;
     private ImageGridBuilder imageGridBuilder;
 
+    @FXML private StackPane root;
+
     public CollectionGrid(PathResolver resolver, ImageGridBuilder imageGridBuilder) {
-        super();
         this.resolver = resolver;
         this.imageGridBuilder = imageGridBuilder;
+
+        loadFXML();
     }
+
+    protected void loadFXML() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(COLLECTION_GRID_FXML));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
 
     public void setCollection(Collection collection) throws IOException {
         this.collection = collection;
         ImageGrid imageGrid = imageGridBuilder.build();
         imageGrid.setImages(getThumbnailsPaths(collection));
-        getChildren().add(imageGrid);
+        root.getChildren().add(imageGrid);
     }
 
     public Collection getCollection() {
