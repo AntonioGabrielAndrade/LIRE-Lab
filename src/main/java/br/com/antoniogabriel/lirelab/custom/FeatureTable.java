78e964cc3f86f7d68b7bad03494088a0e02134f6
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-06-28 17:30:38 -0300

    Remove unecessary method

--- src/main/java/br/com/antoniogabriel/lirelab/custom/FeatureTable.java
+++ src/main/java/br/com/antoniogabriel/lirelab/custom/FeatureTable.java
@@ -5,37 +5,34 @@ import br.com.antoniogabriel.lirelab.util.FeatureUtils;
 import javafx.beans.binding.BooleanBinding;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.List;
-import java.util.ResourceBundle;
 
 
-public class FeatureTable extends TableView<ViewableFeature> implements Initializable {
+public class FeatureTable extends TableView<ViewableFeature> {
 
     public static final String FXML = "table-view.fxml";
 
     @FXML private TableColumn<ViewableFeature, Boolean> selectedCol;
     @FXML private TableColumn<ViewableFeature, String> nameCol;
 
-    FeatureUtils featureUtils = new FeatureUtils();
+    private FeatureUtils featureUtils = new FeatureUtils();
 
     public FeatureTable() throws IOException {
+        loadFXML();
+        setupTableColumns();
+    }
+
+    protected void loadFXML() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(FXML));
         loader.setRoot(this);
         loader.setController(this);
         loader.load();
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        setupTableColumns();
     }
 
     public BooleanBinding noFeatureSelected() {
