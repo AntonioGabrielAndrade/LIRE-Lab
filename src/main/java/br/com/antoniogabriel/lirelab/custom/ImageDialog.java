d3d68c9e6eb9bead18d56e89f97251fa21747852
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-06-30 13:29:34 -0300

    Create ImageDialog custom component

---
+++ src/main/java/br/com/antoniogabriel/lirelab/custom/ImageDialog.java
@@ -0,0 +1,24 @@
+package br.com.antoniogabriel.lirelab.custom;
+
+import br.com.antoniogabriel.lirelab.app.ImageViewFactory;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.image.ImageView;
+
+import java.io.FileNotFoundException;
+
+
+public class ImageDialog extends Dialog<Void> {
+
+    private final ImageViewFactory imageViewFactory = new ImageViewFactory();
+
+    public ImageDialog(String imagePath) throws FileNotFoundException {
+        ImageView originalImageView = imageViewFactory.create(imagePath);
+        originalImageView.setPreserveRatio(true);
+        originalImageView.setFitHeight(800);
+
+        getDialogPane().getButtonTypes().add(ButtonType.OK);
+        getDialogPane().setId("image-dialog");
+        getDialogPane().setContent(originalImageView);
+    }
+}
---
+++ src/test/java/br/com/antoniogabriel/lirelab/custom/ImageDialogAcceptanceTest.java
@@ -0,0 +1,55 @@
+package br.com.antoniogabriel.lirelab.custom;
+
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.io.FileNotFoundException;
+
+import static br.com.antoniogabriel.lirelab.test.TestPaths.TEST_IMAGES;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.util.NodeQueryUtils.isVisible;
+
+public class ImageDialogAcceptanceTest extends ApplicationTest {
+
+    private ImageDialog dialog;
+    private ImageView imageView;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        interact(() -> {
+            try {
+                dialog = new ImageDialog(TEST_IMAGES + "14474347006_99aa0fd981_k.jpg");
+                dialog.show();
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException("Error", e);
+            }
+        });
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        interact(() -> {
+            dialog.close();
+        });
+    }
+
+    @Test
+    public void shouldShowImage() throws Exception {
+        Node imageNode = from(lookup("#image-dialog")).lookup("#14474347006_99aa0fd981_k").query();
+        verifyThat(imageNode, isVisible());
+    }
+
+    @Test
+    public void shouldSHowOkButton() throws Exception {
+        clickOn("OK");
+    }
+}
