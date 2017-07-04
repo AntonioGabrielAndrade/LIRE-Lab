9322ddaead792445354baf3d1e7b75e3a36e8538
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-07-04 11:15:35 -0300

    Add walking skeleton for query functionality

--- src/main/java/br/com/antoniogabriel/lirelab/collection/CollectionService.java
+++ src/main/java/br/com/antoniogabriel/lirelab/collection/CollectionService.java
@@ -2,10 +2,23 @@ package br.com.antoniogabriel.lirelab.collection;
 
 import br.com.antoniogabriel.lirelab.exception.LireLabException;
 import br.com.antoniogabriel.lirelab.lire.Feature;
+import br.com.antoniogabriel.lirelab.util.CollectionUtils;
+import net.semanticmetadata.lire.builders.DocumentBuilder;
+import net.semanticmetadata.lire.searchers.GenericFastImageSearcher;
+import net.semanticmetadata.lire.searchers.ImageSearchHits;
+import net.semanticmetadata.lire.searchers.ImageSearcher;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.store.FSDirectory;
 
+import javax.imageio.ImageIO;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 @Singleton
@@ -62,5 +75,38 @@ public class CollectionService {
 
     public void addCollectionsChangeListener(Runnable callback) {
         collectionsMonitor.addListener(callback);
+    }
+
+    public Collection runQuery(Collection collection, Feature feature, Image queryImage) throws IOException {
+        BufferedImage img = null;
+        boolean passed = false;
+        File f = new File(queryImage.getImagePath());
+        if (f.exists()) {
+            try {
+                img = ImageIO.read(f);
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+
+
+        IndexReader ir = DirectoryReader.open(FSDirectory.open(Paths.get(resolver.getIndexDirectoryPath(collection.getName()))));
+        ImageSearcher searcher = new GenericFastImageSearcher(collection.getImages().size(), feature.getLireClass());
+
+        List<Image> result = new ArrayList<>();
+        CollectionUtils utils = new CollectionUtils(resolver);
+
+        // searching with a image file ...
+        ImageSearchHits hits = searcher.search(img, ir);
+        // searching with a Lucene document instance ...
+        for (int i = 0; i < hits.length(); i++) {
+            String fileName = ir.document(hits.documentID(i)).getValues(DocumentBuilder.FIELD_NAME_IDENTIFIER)[0];
+            Image image = new Image(fileName, utils.getThumbnailPathFromImagePath(collection, fileName));
+            result.add(image);
+            System.out.println(hits.score(i) + ": \t" + fileName);
+        }
+
+        collection.setImages(result);
+        return collection;
     }
 }
