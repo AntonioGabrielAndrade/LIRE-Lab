76f08490f0a9e56d1db529684403f431019e400b
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-07-04 11:21:35 -0300

    Add high level query mechanism

--- src/main/java/br/com/antoniogabriel/lirelab/search/SearchViewController.java
+++ src/main/java/br/com/antoniogabriel/lirelab/search/SearchViewController.java
@@ -1,54 +1,88 @@
 package br.com.antoniogabriel.lirelab.search;
 
 import br.com.antoniogabriel.lirelab.collection.Collection;
+import br.com.antoniogabriel.lirelab.collection.CollectionService;
 import br.com.antoniogabriel.lirelab.collection.Image;
 import br.com.antoniogabriel.lirelab.custom.collectiongrid.CollectionGrid;
 import br.com.antoniogabriel.lirelab.custom.collectiongrid.ImageClickHandler;
-import br.com.antoniogabriel.lirelab.custom.imagegrid.ImageGrid;
+import br.com.antoniogabriel.lirelab.custom.singleimagegrid.ImageChangeListener;
+import br.com.antoniogabriel.lirelab.custom.singleimagegrid.SingleImageGrid;
 import br.com.antoniogabriel.lirelab.exception.LireLabException;
 import br.com.antoniogabriel.lirelab.lire.Feature;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @Singleton
 public class SearchViewController {
 
-    @FXML private ImageGrid queryGrid;
+    @FXML private SingleImageGrid queryGrid;
     @FXML private CollectionGrid collectionGrid;
+
+    private CollectionService service;
+
+    @Inject
+    public SearchViewController(CollectionService service) {
+        this.service = service;
+    }
 
     public void startSearchSession(Collection collection, Feature feature) {
         try {
 
             collectionGrid.setCollection(collection, new SetImageToGridClickHandler(queryGrid));
-
-//            queryGrid.setOnChange(() -> {
-//                service.search(collection.getImages(), feature, queryGrid.getImages());
-//            });
+            queryGrid.setOnChange(new ImageChangeListenerImpl(collection, feature, service, collectionGrid, queryGrid));
 
         } catch (IOException e) {
             throw new LireLabException("Could not show collection", e);
         }
     }
 
-    class SetImageToGridClickHandler implements ImageClickHandler {
+    static class SetImageToGridClickHandler implements ImageClickHandler {
 
-        private final ImageGrid grid;
+        private final SingleImageGrid grid;
 
-        public SetImageToGridClickHandler(ImageGrid grid) {
+        public SetImageToGridClickHandler(SingleImageGrid grid) {
             this.grid = grid;
         }
 
         @Override
         public void handle(Image image, MouseEvent event) {
+            grid.setImage(image);
+        }
+    }
+
+    static class ImageChangeListenerImpl implements ImageChangeListener {
+
+        private final Collection collection;
+        private final Feature feature;
+        private CollectionService service;
+        private CollectionGrid collectionGrid;
+        private SingleImageGrid queryGrid;
+
+        public ImageChangeListenerImpl(Collection collection,
+                                       Feature feature,
+                                       CollectionService service,
+                                       CollectionGrid collectionGrid,
+                                       SingleImageGrid queryGrid) {
+
+            this.collection = collection;
+            this.feature = feature;
+            this.service = service;
+            this.collectionGrid = collectionGrid;
+            this.queryGrid = queryGrid;
+        }
+
+        @Override
+        public void changed(Image queryImage) {
             try {
-                grid.clear();
-                grid.addImage(image.getThumbnailPath());
-            } catch (FileNotFoundException e) {
-                throw new LireLabException("Could not add image to grid", e);
+                collectionGrid.setCollection(
+                        service.runQuery(collection, feature, queryImage),
+                        new SetImageToGridClickHandler(queryGrid));
+            } catch (IOException e) {
+                throw new LireLabException("Could not run query", e);
             }
         }
     }
--- src/test/java/br/com/antoniogabriel/lirelab/search/SearchViewControllerTest.java
+++ src/test/java/br/com/antoniogabriel/lirelab/search/SearchViewControllerTest.java
@@ -1,7 +1,10 @@
 package br.com.antoniogabriel.lirelab.search;
 
 import br.com.antoniogabriel.lirelab.collection.Collection;
+import br.com.antoniogabriel.lirelab.collection.CollectionService;
+import br.com.antoniogabriel.lirelab.collection.Image;
 import br.com.antoniogabriel.lirelab.custom.collectiongrid.CollectionGrid;
+import br.com.antoniogabriel.lirelab.custom.singleimagegrid.SingleImageGrid;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -9,6 +12,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static br.com.antoniogabriel.lirelab.lire.Feature.CEDD;
+import static java.util.Arrays.asList;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
@@ -18,8 +22,10 @@ import static org.mockito.Mockito.verify;
 public class SearchViewControllerTest {
 
     @Mock private CollectionGrid collectionGrid;
+    @Mock private SingleImageGrid queryGrid;
+    @Mock private CollectionService service;
 
-    @InjectMocks private SearchViewController controller = new SearchViewController();
+    @InjectMocks private SearchViewController controller = new SearchViewController(service);
 
     @Test
     public void shouldShowCollectionAndAddImageToQueryPaneWhenClicked() throws Exception {
@@ -29,5 +35,30 @@ public class SearchViewControllerTest {
 
         verify(collectionGrid).setCollection(eq(collection),
                 any(SearchViewController.SetImageToGridClickHandler.class));
+    }
+
+    @Test
+    public void shouldSetQueryChangeListenerToQueryGrid() throws Exception {
+        Collection collection = new Collection();
+
+        controller.startSearchSession(collection, CEDD);
+
+        verify(queryGrid).setOnChange(any(SearchViewController.ImageChangeListenerImpl.class));
+    }
+
+    @Test
+    public void shouldRunQueryWhenQueryImageIsSet() throws Exception {
+        Collection collection = new Collection();
+        collection.setImages(asList(new Image("path1", "path1"),
+                                    new Image("path2", "path2")));
+
+        Image queryImage = new Image("", "");
+
+        SearchViewController.ImageChangeListenerImpl listener =
+                new SearchViewController.ImageChangeListenerImpl(collection, CEDD, service, collectionGrid, queryGrid);
+
+        listener.changed(queryImage);
+
+        verify(service).runQuery(collection, CEDD, queryImage);
     }
 }
