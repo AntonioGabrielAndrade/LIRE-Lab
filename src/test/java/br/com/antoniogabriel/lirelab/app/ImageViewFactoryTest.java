39238cad863e035006669b2867977baba94e3af6
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-07-04 11:13:47 -0300

    Load ImageView in background

--- src/test/java/br/com/antoniogabriel/lirelab/app/ImageViewFactoryTest.java
+++ src/test/java/br/com/antoniogabriel/lirelab/app/ImageViewFactoryTest.java
@@ -1,10 +1,9 @@
 package br.com.antoniogabriel.lirelab.app;
 
+import javafx.embed.swing.JFXPanel;
 import javafx.scene.image.ImageView;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.io.FileNotFoundException;
 
 import static junit.framework.TestCase.assertNotNull;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -13,7 +12,7 @@ import static org.junit.Assert.assertTrue;
 
 public class ImageViewFactoryTest {
 
-    private static final String INEXISTENT_FILE_PATH = "some/path/that/dont/exists";
+    private static final JFXPanel INIT_JAVAFX = new JFXPanel();
 
     private static final String EXISTENT_FILE_PATH = "thumb/16903390174_1d670a5849_h.thumbnail.jpg";
     private static final String EXISTENT_FILE_NAME_WITH_NO_EXTENSIONS = "16903390174_1d670a5849_h";
@@ -23,11 +22,6 @@ public class ImageViewFactoryTest {
     @Before
     public void setUp() throws Exception {
         factory = new ImageViewFactory();
-    }
-
-    @Test(expected = FileNotFoundException.class)
-    public void shouldThrowExceptionWhenFileDontExists() throws Exception {
-        factory.create(INEXISTENT_FILE_PATH);
     }
 
     @Test
