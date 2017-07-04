4a467570b2e33127ff305717e6676928de2e0d87
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-07-04 18:10:27 -0300

    Add setImages(List<Image>) API

--- src/main/java/br/com/antoniogabriel/lirelab/custom/collectiongrid/CollectionGrid.java
+++ src/main/java/br/com/antoniogabriel/lirelab/custom/collectiongrid/CollectionGrid.java
@@ -11,6 +11,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
+import java.util.List;
 
 public class CollectionGrid extends StackPane {
 
@@ -45,8 +46,16 @@ public class CollectionGrid extends StackPane {
 
     public void setCollection(Collection collection, ImageClickHandler handler) throws IOException {
         this.collection = collection;
+        setImages(collection.getImages(), handler);
+    }
+
+    public void setImages(List<Image> images) throws IOException {
+        setImages(images, new DisplayImageDialogHandler(new DialogProvider(), new FileUtils()));
+    }
+
+    public void setImages(List<Image> images, ImageClickHandler handler) throws IOException {
         grid.clear();
-        for (Image image : collection.getImages()) {
+        for (Image image : images) {
             ImageView imageView = grid.addImage(image.getThumbnailPath());
             toolTipProvider.setToolTip(imageView, image.getImageName());
             imageView.setOnMouseClicked(eventHandlerFactory.createFrom(image, handler));
