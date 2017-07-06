40ba2b513dc2ebd1054ca6e49794902c94d0be2d
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-07-06 16:56:55 -0300

    Remove duplication

--- src/test/java/br/com/antoniogabriel/lirelab/test/AsyncUtils.java
+++ src/test/java/br/com/antoniogabriel/lirelab/test/AsyncUtils.java
@@ -3,23 +3,23 @@ package br.com.antoniogabriel.lirelab.test;
 import javafx.beans.value.ObservableBooleanValue;
 import javafx.scene.Node;
 import org.testfx.api.FxRobot;
-import org.testfx.util.WaitForAsyncUtils;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeoutException;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.testfx.util.WaitForAsyncUtils.waitFor;
 
 public class AsyncUtils {
 
     private static final TimeOut DEFAULT_TIMEOUT = TimeOut.of(5, SECONDS);
 
     public static void waitUntil(ObservableBooleanValue condition) throws TimeoutException {
-        waitUntil(condition, TimeOut.of(5, SECONDS));
+        waitUntil(condition, DEFAULT_TIMEOUT);
     }
 
     public static void waitUntil(ObservableBooleanValue condition, TimeOut timeout) throws TimeoutException {
-        WaitForAsyncUtils.waitFor(timeout.value, timeout.unit, condition);
+        waitFor(timeout.value, timeout.unit, condition);
     }
 
     public static void waitUntil(Callable<Boolean> condition) throws TimeoutException {
@@ -27,28 +27,38 @@ public class AsyncUtils {
     }
 
     public static void waitUntil(Callable<Boolean> condition, TimeOut timeout) throws TimeoutException {
-        WaitForAsyncUtils.waitFor(timeout.value, timeout.unit, condition);
+        waitFor(timeout.value, timeout.unit, condition);
     }
 
     public static void waitUntilIsVisible(String query) throws TimeoutException {
-        waitUntil(() -> {
-            Node node = (new FxRobot()).lookup(query).query();
-            return node == null ? false : node.isVisible();
-        });
+        waitUntilVisibilityIs(query, true);
     }
 
     public static void waitUntilIsVisible(String query, String queryFrom) throws TimeoutException {
-        waitUntil(() -> {
-            FxRobot robot = new FxRobot();
-            Node node = robot.from(robot.lookup(queryFrom)).lookup(query).query();
-            return node == null ? false : node.isVisible();
-        });
+        waitUntilVisibilityIs(query, queryFrom, true);
+    }
+
+    public static void waitUntilIsNotVisible(String query, String queryFrom) throws TimeoutException {
+        waitUntilVisibilityIs(query, queryFrom, false);
     }
 
     public static void waitUntilIsNotVisible(String query) throws TimeoutException {
+        waitUntilVisibilityIs(query, false);
+    }
+
+    public static void waitUntilVisibilityIs(String query, String queryFrom, boolean visibility) throws TimeoutException {
         waitUntil(() -> {
-            Node node = (new FxRobot()).lookup(query).query();
-            return node == null ? true : !node.isVisible();
+            FxRobot robot = new FxRobot();
+            Node node = robot.from(robot.lookup(queryFrom)).lookup(query).query();
+            return (node == null) ? (!visibility) : (node.isVisible() == visibility);
+        });
+    }
+
+    public static void waitUntilVisibilityIs(String query, boolean visibility) throws TimeoutException {
+        waitUntil(() -> {
+            FxRobot robot = new FxRobot();
+            Node node = robot.lookup(query).query();
+            return (node == null) ? (!visibility) : (node.isVisible() == visibility);
         });
     }
 }
