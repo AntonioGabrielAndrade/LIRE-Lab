210ecdf480963d110954b4a74cc0c78b21969d24
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-07-26 11:29:32 -0300

    Remove unused code

--- src/main/java/br/com/antoniogabriel/lirelab/collection/DialogProvider.java
+++ src/main/java/br/com/antoniogabriel/lirelab/collection/DialogProvider.java
@@ -1,12 +1,8 @@
 package br.com.antoniogabriel.lirelab.collection;
 
-import br.com.antoniogabriel.lirelab.custom.ChooseFeatureDialog;
 import br.com.antoniogabriel.lirelab.custom.image_dialog.ImageDialog;
 import br.com.antoniogabriel.lirelab.custom.progress_dialog.ProgressDialog;
-import br.com.antoniogabriel.lirelab.lire.Feature;
-import javafx.application.Platform;
 import javafx.concurrent.Task;
-import javafx.embed.swing.JFXPanel;
 import javafx.event.Event;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
@@ -16,10 +12,7 @@ import javafx.stage.Window;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.List;
 
-import static br.com.antoniogabriel.lirelab.lire.Feature.*;
-import static java.util.Arrays.asList;
 import static javafx.scene.control.Alert.AlertType.CONFIRMATION;
 import static javafx.scene.control.ButtonType.OK;
 
@@ -56,24 +49,6 @@ public class DialogProvider {
         ImageDialog dialog = new ImageDialog(imagePath);
         dialog.initOwner(owner);
         return dialog;
-    }
-
-    public Feature chooseFeatureFrom(Collection collection, Window owner) {
-        List<Feature> features = collection.getFeatures();
-        ChooseFeatureDialog dialog = new ChooseFeatureDialog(features);
-        dialog.initOwner(owner);
-        return dialog.showAndGetFeature();
-    }
-
-    public static void main(String[] args) {
-        new JFXPanel();
-        Platform.runLater(() -> {
-            Collection collection = new Collection("Collection");
-            collection.setFeatures(asList(CEDD, TAMURA, FCTH, COLOR_HISTOGRAM));
-            Feature feature = new DialogProvider().chooseFeatureFrom(collection, null);
-            System.out.println("selected feature: " + feature);
-        });
-
     }
 
     public boolean confirmDeleteCollection() {
