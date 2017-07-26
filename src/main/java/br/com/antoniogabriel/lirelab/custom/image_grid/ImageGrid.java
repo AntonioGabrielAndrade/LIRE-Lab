d4667ae9230f7e9211f9e5657fe94ba86d54715a
Antonio Gabriel <gabriel.ecomp@gmail.com>
2017-07-26 16:12:00 -0300

    Adjust padding when changing gap

--- src/main/java/br/com/antoniogabriel/lirelab/custom/image_grid/ImageGrid.java
+++ src/main/java/br/com/antoniogabriel/lirelab/custom/image_grid/ImageGrid.java
@@ -7,6 +7,7 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
@@ -15,6 +16,8 @@ import javafx.scene.layout.StackPane;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.util.List;
+
+import static javafx.beans.binding.Bindings.createObjectBinding;
 
 public class ImageGrid extends StackPane {
 
@@ -55,6 +58,7 @@ public class ImageGrid extends StackPane {
     public void bindGapsTo(DoubleProperty property) {
         flowPane.hgapProperty().bind(property);
         flowPane.vgapProperty().bind(property);
+        flowPane.paddingProperty().bind(createObjectBinding(() -> new Insets(property.doubleValue()), property));
     }
 
 
